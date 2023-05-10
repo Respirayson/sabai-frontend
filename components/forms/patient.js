978262b978e1f9e2dc02240af7c45a7434ec9a3f
Jordan Yoong <jordanyoong@yahoo.com.sg>
2023-05-10 17:00:46 +0800

    add default selected for diagnostic

--- components/forms/patient.js
+++ components/forms/patient.js
@@ -366,9 +366,9 @@ class MedicalForm extends React.Component {
               <select
                 name="referred_for"
                 onChange={handleInputChange}
-                default="Diagnostic"
+                selected="Diagnostic"
               >
-                <option value="Diagnostic">Diagnostic</option>
+                <option value="Diagnostic" selected>Diagnostic</option>
                 <option value="Acute">Acute</option>
                 <option value="Chronic">Chronic</option>
               </select>

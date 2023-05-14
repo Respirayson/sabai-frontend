318316e8b3927fdd7628a99e948b0bb0bbf23721
ShawnChew <shawnchew200@gmail.com>
2023-05-14 09:35:19 +0800

    Remove console.log and sort in forms/patient.js

--- components/forms/patient.js
+++ components/forms/patient.js
@@ -534,12 +534,6 @@ class PrescriptionForm extends React.Component {
       onSubmit,
       isEditing,
     } = this.props;
-    console.log(medicationOptions);
-    console.log(medicationOptions[0].props.children);
-    medicationOptions.sort(
-      (a, b) => (a, b) => a.props.children.localeCompare(b.props.children)
-    );
-    console.log(medicationOptions);
     return (
       <div className="column is-12">
         <h1 style={{ color: "black", fontSize: "1.5em" }}>Prescription</h1>
